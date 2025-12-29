f524acaa62db16cbae5ffec1e5e4addc4c7bd23d
Aakash KT <aakash.kt@research.iiit.ac.in>
2025-12-29 12:24:55 +0100

    Added both shared and static lib options

--- scripts/build.py
+++ scripts/build.py
@@ -4,21 +4,27 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--driver', help='Which driver to use? (see driver/ folder)', default='cuda', required=True)
     parser.add_argument('--device', help='Which device to use? (see device/ folder)', default='ptx_gpu_1', required=True)
-    parser.add_argument('--clean', help='yes | no', default='no')
+    parser.add_argument('--clean', action="store_true")
+    parser.add_argument('--static_lib', action="store_true")
     args = parser.parse_args()
 
     utils.init()
 
-    if args.clean.lower() == 'yes':
+    if args.clean:
         shutil.rmtree('build')
         os.mkdir('build')
     
-    if not os.path.exists('build'):
+    path_exists = os.path.exists('build')
+    if not path_exists:
         os.mkdir('build')
 
     os.chdir('build')
 
-    if args.clean.lower() == 'yes':
-        os.system('cmake .. -DDRIVER=%s -DDEVICE=%s -DCMAKE_BUILD_TYPE=Release' % (args.driver.lower(), args.device.lower()))
-        
+    if args.clean or (not path_exists):
+
+        if args.static_lib:
+            os.system('cmake .. -DPRESM_SHARED_LIB=FALSE -DDRIVER=%s -DDEVICE=%s -DCMAKE_BUILD_TYPE=Release' % (args.driver.lower(), args.device.lower()))
+        else:
+            os.system('cmake .. -DPRESM_SHARED_LIB=TRUE -DDRIVER=%s -DDEVICE=%s -DCMAKE_BUILD_TYPE=Release' % (args.driver.lower(), args.device.lower()))
+
     os.system('cmake --build . --config Release --target install')
